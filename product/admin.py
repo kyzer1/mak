9cfea20771fbdaeaa9972426cc99e3968e2fdf77
kyzer1 <94113743+kyzer1@users.noreply.github.com>
2021-12-18 18:41:32 +0330

    Delete product directory

--- product/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from product.models import CategoryProduct, Product, SubCategoryProduct
-
-admin.site.register(CategoryProduct)
-admin.site.register(SubCategoryProduct)
-admin.site.register(Product)
