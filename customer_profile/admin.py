eb4a028783b3b4952603f18e0e5067c28a2e2a4d
kyzer1 <94113743+kyzer1@users.noreply.github.com>
2021-12-18 18:40:48 +0330

    Delete customer_profile directory

--- customer_profile/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomerProfile, CustomerAddress, HistoryCustomer, Favotite
-
-admin.site.register(CustomerProfile)
-admin.site.register(CustomerAddress)
-admin.site.register(HistoryCustomer)
-admin.site.register(Favotite)
