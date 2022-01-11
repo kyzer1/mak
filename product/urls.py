52f3da22a2c2cb65913f2d6cd32e6cd306d74983
kyzer1 <kasra.nrz95@gmail.com>
2022-01-11 11:41:57 +0330

    homepage subcats subcatproducts

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import products
+from .views import show_sub_cat, show_sub_cat_det, show_product
 
 app_name = 'product'
 
 urlpatterns = [
-    path('product/', products, name='products')
-    
+
+    path('sub_cat/', show_sub_cat, name='sub_cat'),
+    path('sub_cat_det/<str:cat>', show_sub_cat_det, name='sub_cat_det'),
+    path('show_product/<str:sub_cat>', show_product, name='show_product'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from .models import CategoryProduct, Product
 
 
-def products(request):
-    ctx = {}
-    return render(request, 'product/base_products.html', ctx)
+def show_sub_cat(request): # --> showing sub_cat after click 
+    sub = []
+    parent_category = CategoryProduct.objects.all().filter(parent=None)
+
+    for cat in parent_category:
+        sub.append(cat.child_cat.all())
+
+    ctx = {
+        'sub_category': sub
+    }
+
+    return render(request, 'product/sub_cat.html', ctx)
+
+
+def show_sub_cat_det(request, cat): # --> showing sub_cat after click 
+    sub = CategoryProduct.objects.filter(parent__title=cat)
+
+    ctx = {
+        'sub_category': sub
+    }
+
+    return render(request, 'product/sub_cat.html', ctx)
+
+
+def show_product(request, sub_cat):
+    product = Product.objects.all().filter(cat__title=sub_cat)
+
+    ctx = {
+        'product': product
+    }
+
+    return render(request, 'product/detail_sub_cat.html', ctx)
+
+    
