a3d7cc3c7c7cdf1d548dd185789d63c5bc626944
amir dehghan <dehghan215@gmail.com>
2022-01-16 15:45:12 +0330

    amir filtering naghes

--- product/views.py
+++ product/views.py
@@ -1,10 +1,10 @@
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import CategoryProduct, Product
 from django.views.generic import ListView
-
+from django.db.models import Q
+from django.core.cache import cache
+from supply.models import SalesmanProduct
 
 def show_sub_cat(request): # --> showing sub_cat after click 
     sub = []
@@ -52,9 +52,75 @@ class ShowProductCategory(ListView):
         return super().get(self, request, *args, **kwargs)
 
 
+
+class FilteringAll(ListView):
+    model = SalesmanProduct
+    template_name = 'product/side_bar_filtering.html'
+    context_object_name = "productlist"
+    paginate_by = 8
+
+    def get_queryset(self):
+        queryst =  super().get_queryset()
+        filter_by_price_1 = self.request.GET.get("price1")
+        filter_by_price_2 = self.request.GET.get("price2")
+        if filter_by_price_1 and filter_by_price_2:
+            queryst = SalesmanProduct.objects.filter(Q(price__gte=float(filter_by_price_2)) & Q(price__lte=float(filter_by_price_1)))
+            cache.set('filtering_price', queryst, 5)
+            # print("cache = ", queryst)
+            # print(queryst)
+
+            # for elm in queryst:
+            #     print(elm.product.title)
+
+            # print('my  query =====> ',queryst.get(product=Product.objects.get(id=3)))
+        return queryst
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        top_cat = CategoryProduct.objects.filter(parent__isnull=True)
+        top_cat_2 = CategoryProduct.objects.filter(parent__isnull=False)
+        context["category"] = top_cat
+        context["sub_category"] = top_cat_2
+
+        return context
+    
+    
+
+
+
+
+
 class ProductList(ListView):
     model = Product
     template_name = 'product/list_products.html'
     context_object_name ='products'
-    paginate_by = 16
+    paginate_by = 10
 
+    # def get_queryset(self):
+    #     self.queryset = super().get_queryset()
+    #     filtering_by_price = cache.get("filtering_price")
+
+    #     # a = filtering_by_price.values(product=1)
+    #     # print("cache:", filtering_by_price.all())
+    #     # print(a in qs)        
+    #     if filtering_by_price is None:
+    #         self.queryset = Product.objects.all()
+    #     else:
+    #         self.queryset = filtering_by_price
+    #         for elm in self.queryset:
+    #             print(elm.product_id)
+    #         # q = list(Product.objects.filter(products__id=filtering_by_price))
+    #     return self.queryset
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        filtering_by_price = cache.get("filtering_price")
+        if filtering_by_price:
+            my_dic = {}
+            for elm in filtering_by_price:
+                my_dic[elm.product.id]=elm.product
+            context["my_products"] = my_dic.values
+        else:
+            pass
+        return context
+    
